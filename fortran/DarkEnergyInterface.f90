9af8aa99ebd5b8c9a5c4ba63765fc661e307189e
minegokcen <51970327+minegokcen@users.noreply.github.com>
2025-09-30 07:44:05 -0700

    Added PDL + neg CC. Modified w(a)

--- fortran/DarkEnergyInterface.f90
+++ fortran/DarkEnergyInterface.f90
@@ -192,6 +192,17 @@
 
     if(.not. this%use_tabulated_w) then
         TDarkEnergyEqnOfState_w_de= this%w_lam+ this%wa*(1._dl-a)
+        
+    !! MG mods for PDL + neg CC begin
+        real(dl) :: a_c !define critial scale factor as a real valued variable
+        a_c = 1._dl + (1._dl + this%w_lam)/(this%wa)
+        if(a>=a_c) then
+            TDarkEnergyEqnOfState_w_de= -1._dl ! neg CC
+       ! else
+       !     TDarkEnergyEqnOfState_w_de= this%w_lam+ this%wa*(1._dl-a)
+       ! endif
+    !! MG mods for PDL + neg CC end
+    
     else
         al=dlog(a)
         if(al <= this%equation_of_state%Xmin_interp) then
